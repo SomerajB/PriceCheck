b703dae247e3997e685c0428ae7fb343a5e45fa9
Someraj Bose <somerajbose@Somerajs-Laptop.local>
2024-09-15 14:53:58 +1000

    shortened code

--- someraj_tests/price_check.spec.js
+++ someraj_tests/price_check.spec.js
@@ -2,17 +2,24 @@
 const { test, expect } = require('@playwright/test');
 
 test('check price change for iphone 15', async ({ page }) => {
-  await page.goto('https://www.jbhifi.com.au/');
+  await page.goto('https://www.jbhifi.com.au/products/apple-iphone-15-128gb-black');
 
   
   // await page.waitForTimeout(10000)
 
-  await page.getByRole('button', { name: 'Products' }).click();
-  await page.getByRole('menuitem', { name: 'Mobile Phones' }).click();
-  await page.getByRole('menuitem', { name: 'Apple iPhone' }).click();
-  await page.getByRole('menuitem', { name: 'iPhone 15 series' }).click();
-  await page.getByRole('link', { name: 'Apple iPhone 15 128GB (Black) iPhone' }).click();
-  await page.getByRole('link', { name: 'Apple iPhone 15 128GB (Black) Apple iPhone 15 128GB (Black) 4.6 (276)' }).click();
+  // await page.getByRole('button', { name: 'Products' }).click();
+  // await page.getByRole('menuitem', { name: 'Mobile Phones' }).click();
+  // await page.getByRole('menuitem', { name: 'Apple iPhone' }).click();
+  // await page.getByRole('menuitem', { name: 'iPhone 15 series' }).click();
+  // await page.getByRole('link', { name: 'Apple iPhone 15 128GB (Black) iPhone' }).click();
+  // const item = await page.locator('div[data-testid="product-card-title"]').all()
+  // for (const i of item){
+  //   const prod = await i.textContent()
+  //   if (prod == 'Apple iPhone 15 128GB (Black)'){
+  //     i.click()
+  //     break;
+  //   }
+  // }
   const present_price = await page.locator('#pdp-price-cta span').last().textContent()
   expect (Number(present_price)).toEqual(1247)
 });
