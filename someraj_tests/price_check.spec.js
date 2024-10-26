24e9550e32a68116b2f8a72059b09966fc407821
SomerajB <bosesomeraj@gmail.com>
2024-10-27 01:35:59 +1100

    Update price_check.spec.js
    Changed the reduced price of ipad

--- someraj_tests/price_check.spec.js
+++ someraj_tests/price_check.spec.js
@@ -17,12 +17,12 @@ test('check price change for iphone 15 in OfficeWorks', async ({ page }) => {
 test('check price change for ipad 10 in JB-HiFi', async ({ page }) => {
   await page.goto('https://www.jbhifi.com.au/products/apple-ipad-10-9-inch-64gb-wi-fi-blue-10th-gen');
   const present_price = await page.locator('#pdp-price-cta span').last().textContent()
-  expect (Number(present_price)).toEqual(587)
+  expect (Number(present_price)).toEqual(517)
 });
 
 test('check price change for ipad 10 in OfficeWorks', async ({ page }) => {
   await page.goto('https://www.officeworks.com.au/shop/officeworks/p/ipad-10th-gen-10-9-wifi-64gb-blue-ipad10gen1');
   const present_price = await page.locator('span[data-ref$="product-price-isNotRR"] span').innerHTML()
   const price = present_price.split('$')[1]
-  expect (Number(price)).toEqual(587)
+  expect (Number(price)).toEqual(517)
 });
